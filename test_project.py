7146818b01f968b194987e5fbc4e43a751a1ea27
Antonio Sargeni <antsarg@gmail.com>
2024-02-27 20:57:18 +0100

    added test_search_words

--- test_project.py
+++ test_project.py
@@ -1,8 +1,21 @@
+import pytest
 from project import search_words,  load_list
 
+
+@pytest.fixture
+def unscored_list():
+    return load_list("assets\\spreadthewordlist_unscored_high.txt")
+
 def test_load_list():
-    global word_list
     word_list = load_list("assets\\spreadthewordlist_unscored_high.txt")
     assert len(word_list) > 0
-    assert ("abdul" in word_list) == True
-    assert ("anuglymind" in word_list) == False
+    assert "abdul" in word_list
+    assert "anuglymind" not in word_list
+
+def test_search_words(unscored_list: list):
+    assert search_words("air???ne", unscored_list) == ["airborne", "airplane"]
+    assert search_words("mar?io", unscored_list) == []
+    assert "monitor" in search_words("m???t??", unscored_list)
+    assert "terrain" in search_words("t??ra??", unscored_list)
+    assert "score" not in search_words("st?r?", unscored_list)
+    assert "darkroom" not in search_words("dar??", unscored_list)
